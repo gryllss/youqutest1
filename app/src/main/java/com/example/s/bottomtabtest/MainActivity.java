52adbacc6f284fc5ec1549d90a48cd0d9ae60fc4
Bear <943675392@qq.com>
2018-09-03 12:07:15 +0800

    返回

--- app/src/main/java/com/example/s/bottomtabtest/MainActivity.java
+++ app/src/main/java/com/example/s/bottomtabtest/MainActivity.java
@@ -108,12 +108,29 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    private long exitTime=0;
+
+    @Override
+    public boolean onKeyDown(int keyCode,KeyEvent event){
+        if(keyCode==KeyEvent.KEYCODE_BACK){
+            exit();
+            return false;
+        }
+        return super.onKeyDown(keyCode,event);
+    }
+
+    private void exit() {
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            Toast.makeText(getApplicationContext(),
+                    "再按一次退出应用", Toast.LENGTH_SHORT).show();
+            exitTime = System.currentTimeMillis();
+        }else{
+                finish();
+                System.exit(0);
+            }
 
 
-
-
-
-
+        }
 
 
 
